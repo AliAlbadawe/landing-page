594f2e81e64cf260703dbfd5ebbce79481ca7a43
Ali Al-Badawe <ali.elbadawe@gmail.com>
2022-01-11 22:44:30 +0200

    commit Final Changes
    Updating main branch with project reqs.
    :)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # landing-page
- Udacity course Project
+
+Udacity course Project.
+
+** January Cohort Web **
+
+---
+
+Tried my best to write the minimal codes number required. To make it the project redable and easy to navigate.
--- js/app.js
+++ js/app.js
@@ -4,51 +4,62 @@
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
  */
 
 // Build menu
 
-// Scroll to section on link click
+let navBar = document.getElementById("navbar__list");
+/*used All to return variable as a NodeList to count it's number*/
+/* Looped over the NodeList and added text with dynamic numbers to make it easy for future adding content*/
+let sections = document.querySelectorAll("section");
+function addNavList() {
+  for (i = 1; i <= sections.length; i++) {
+    let navBarList = document.createElement("li");
+    navBarList.innerHTML = `<a class=menu__link href=#section${i}>Section${i}</a>`;
+    navBar.appendChild(navBarList);
+  }
+}
+/*Listening for DOM loading and then firing the event to increase page performance and minimize repaint and reflow*/
+document.addEventListener("DOMContentLoaded", addNavList());
 
-// Set sections as active
+// Add class 'active' to section when near top of viewport
+let viewCheck = function () {
+  for (i = 0; i < sections.length; i++) {
+    let sec = sections[i];
+    let bound = sec.getBoundingClientRect();
+    if (bound.top >= -100 && bound.bottom <= window.innerHeight) {
+      sec.classList.add("your-active-class");
+    } else {
+      sec.classList.remove("your-active-class");
+    }
+  }
+};
+window.addEventListener("scroll", viewCheck);
+
+//Added "to top" button
+let toTopBtn = document.querySelector(".to-top-btn");
+window.onscroll = () => {
+  if (window.scrollY > 450) {
+    toTopBtn.classList.add("get");
+  } else {
+    toTopBtn.classList.remove("get");
+  }
+};
+
+toTopBtn.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
+});
+
+//Added event for hiding navigation bar when scrolling down and show when scrolling up
+let navParent = document.querySelector(".page__header");
+console.log(navParent);
+let scrollNow = window.scrollY;
+window.onscroll = () => {
+  let scroll2 = window.scrollY;
+  if (scrollNow < scroll2 && window.scrollY > 450) {
+    navParent.classList.add("get");
+  } else {
+    navParent.classList.remove("get");
+  }
+  scrollNow = scroll2;
+};
