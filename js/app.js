cd6e688e9c0ea3f8252c139704f54668690bf655
Ali Al-Badawe <ali.elbadawe@gmail.com>
2022-01-13 00:50:04 +0200

    Update Project
    Used scrollIntoView and scrollTo methods to apply smoot scrolling without using CSS
    Also added the Highlight feature to Navbar ITems when scrolling to the current section in viewport

--- js/app.js
+++ js/app.js
@@ -23,14 +23,19 @@ function addNavList() {
 document.addEventListener("DOMContentLoaded", addNavList());
 
 // Add class 'active' to section when near top of viewport
+//Also added Highlight Feature for Navbar items when current section is in viewport
 let viewCheck = function () {
   for (i = 0; i < sections.length; i++) {
+    let navItems = document.querySelectorAll("a");
+    let newITem = navItems[i];
     let sec = sections[i];
     let bound = sec.getBoundingClientRect();
     if (bound.top >= -100 && bound.bottom <= window.innerHeight) {
       sec.classList.add("your-active-class");
+      newITem.classList.add("get");
     } else {
       sec.classList.remove("your-active-class");
+      newITem.classList.remove("get");
     }
   }
 };
@@ -40,7 +45,7 @@ window.addEventListener("scroll", viewCheck);
 let toTopBtn = document.querySelector(".to-top-btn");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 450) {
+  if (window.scrollY >= 450) {
     toTopBtn.classList.add("get");
   } else {
     toTopBtn.classList.remove("get");
@@ -48,12 +53,14 @@ window.addEventListener("scroll", () => {
 });
 
 toTopBtn.addEventListener("click", () => {
-  document.documentElement.scrollTop = 0;
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  }); /*Added Smooth Behavior without using CSS*/
 });
 
 //Added event for hiding navigation bar when scrolling down and show when scrolling up
 let navParent = document.querySelector(".page__header");
-console.log(navParent);
 let scrollNow = window.scrollY;
 window.onscroll = () => {
   let scroll2 = window.scrollY;
@@ -64,3 +71,14 @@ window.onscroll = () => {
   }
   scrollNow = scroll2;
 };
+
+// smooth scrolling without using CSS -- Thanks for feedback
+document.querySelectorAll("li a").forEach((aElement) => {
+  aElement.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(aElement.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
